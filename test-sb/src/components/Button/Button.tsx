7dedd0af64e57db6210a841642561088efc65ca2
Nanges <olivier.lahaye@oniryx.be>
2024-11-22 22:56:24 +0100

    Adapt button

--- test-sb/src/components/Button/Button.tsx
+++ test-sb/src/components/Button/Button.tsx
@@ -1,22 +1,25 @@
-import { ButtonHTMLAttributes } from "react";
+import { PropsWithChildren } from "react";
 import styles from "./Button.module.css";
 import cn from 'classnames';
 
 type ButtonVariant = 'Primary' | 'Secondary';
 
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+// export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+//     variant?: ButtonVariant;
+// }
+
+export type ButtonProps = PropsWithChildren<{
     variant?: ButtonVariant;
-}
+    className?:string;
+}>;
 
 export function Button(props: ButtonProps){
-    const {type, className, ...remaining} = props;
-    const defaultType = type ? 'button' : type;
     const variant: ButtonVariant = props.variant ?? 'Primary';
     const variantClass = styles[variant];
 
-    return <button 
-        type={defaultType}
-        className={cn(className, styles.Button, variantClass)} 
-        {...remaining}
+    return <button
+        type='button'
+        className={cn(props.className, styles.Button, variantClass)}
+        children={props.children} 
     />
 }
